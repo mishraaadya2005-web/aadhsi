f52fe3616093d064c748e732ec0a26db14bc971b
Aadya Mishra <mishraaadya2005@gmail.com>
2026-05-25 01:14:41 +0530

    Connected Supabase database

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
+import { useEffect } from 'react'
+import { getProducts } from './services/products'
+
 function App() {
+
+  useEffect(() => {
+    async function fetchProducts() {
+      const products = await getProducts()
+      console.log(products)
+    }
+
+    fetchProducts()
+  }, [])
+
   return (
     <div className="text-4xl font-bold text-center mt-20">
-      Aadshi Website
+      Aadshi Backend Connected
     </div>
   )
 }
---
+++ client/src/services/products.js
@@ -0,0 +1,14 @@
+import { supabase } from './supabase'
+
+export async function getProducts() {
+  const { data, error } = await supabase
+    .from('products')
+    .select('*')
+
+  if (error) {
+    console.log(error)
+    return []
+  }
+
+  return data
+}
