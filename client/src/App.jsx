350b2781351b0ed5bd6e10fa0ab779ecafeb322a
Aadya Mishra <mishraaadya2005@gmail.com>
2026-06-02 00:48:25 +0530

    Connected Supabase products successfully

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,28 @@
 import { Routes, Route } from 'react-router-dom'
+
 import Home from './pages/Home'
 import Shop from './pages/Shop'
+import ProductDetails from './pages/ProductDetails'
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/shop" element={<Shop />} />
+
+      <Route
+        path="/"
+        element={<Home />}
+      />
+
+      <Route
+        path="/shop"
+        element={<Shop />}
+      />
+
+      <Route
+        path="/product/:id"
+        element={<ProductDetails />}
+      />
+
     </Routes>
   )
 }
