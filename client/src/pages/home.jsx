b02688da7e2957c402df2db242483c13a0edf18b
Aadya Mishra <mishraaadya2005@gmail.com>
2026-07-09 20:28:20 +0530

    fix: recreate Categories component to reset Git cache

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,6 +1,6 @@
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
-import Categories from "../components/categories";
+import Categories from "../components/Categories";
 import FeaturedProducts from "../components/FeaturedProducts";
 import Reviews from "../components/Reviews";
 import CustomizeCTA from "../components/CustomizeCTA";
